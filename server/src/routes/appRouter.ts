f339837821572621e45132b60153f9867915d646
Nahid Mahbub <nahidshahin@yahoo.com>
2025-05-21 23:15:43 -0500

    Added swagger doc

--- server/src/routes/appRouter.ts
+++ server/src/routes/appRouter.ts
@@ -3,12 +3,89 @@ import productController from '../controller/productController';
 import reviewController from '../controller/reviewController';
 
 const appRouter = express.Router();
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: List of all products with pagination
+ */
 appRouter.get('/', productController.getAllProducts);
+
+/**
+ * @swagger
+ * /products/search:
+ *   get:
+ *     summary: Search products by name
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: List of all products matches the name
+ */
 appRouter.get('/search', productController.searchProducts);
+
+/**
+ * @swagger
+ * /products/:id:
+ *   get:
+ *     summary: Get product by id
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: Get product by it's ID
+ */
 appRouter.get('/:id', productController.getProduct);
+
+/**
+ * @swagger
+ * /products/:id/reviews:
+ *   get:
+ *     summary: Get all reviews of a particular product
+ *     tags: [Reviews]
+ *     responses:
+ *       200:
+ *         description: Get all reviews of a particular product
+ */
 appRouter.get('/:id/reviews', reviewController.getAllReviews);
+
+/**
+ * @swagger
+ * /products/:id/reviews:
+ *   post:
+ *     summary: Create a reviews of a particular product
+ *     tags: [Reviews]
+ *     responses:
+ *       201:
+ *         description: Successfully create a review of a particular product
+ */
 appRouter.post('/:id/reviews', reviewController.saveReview );
+
+/**
+ * @swagger
+ * /products/:id/reviews/:rid:
+ *   put:
+ *     summary: Update a reviews of a particular product
+ *     tags: [Reviews]
+ *     responses:
+ *       200:
+ *         description: Successfully updated a review of a particular product
+ */
 appRouter.put('/:id/reviews/:rid', reviewController.updateReview);
+
+/**
+ * @swagger
+ * /products/:id/reviews/:rid:
+ *   delete:
+ *     summary: Delete a reviews of a particular product
+ *     tags: [Reviews]
+ *     responses:
+ *       200:
+ *         description: Successfully deleted a review of a particular product
+ */
 appRouter.delete('/:id/reviews/:rid', reviewController.deleteReview);
 
 export default appRouter;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 import reviewController from './controller/reviewController';
 import appRouter from './routes/appRouter';
@@ -9,23 +9,34 @@ import appRouter from './routes/appRouter';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
-app.use(helmet());
-app.use(morgan('dev'));
 app.use(express.json());
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Product review backend',
+      version: '1.0.0',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:${PORT}`,
+    },
+  ],  
+  apis: ['./src/routes/*.ts'],
+};
+const swaggerSpec = swaggerJsdoc(options);
 
-// Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// API Endpoints
-
 app.use('/products', appRouter);
 app.post('/seed', reviewController.seedData);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
